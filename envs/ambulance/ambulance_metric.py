2e9b0b39e30cc0f8c734bbca1044d9326dc14fa1
carrierucker <cmr284@cornell.edu>
2021-05-06 17:19:24 -0400

    changed screen size

--- envs/ambulance/ambulance_metric.py
+++ envs/ambulance/ambulance_metric.py
@@ -185,8 +185,8 @@ class AmbulanceEnvironment(gym.Env):
 
   def render(self, mode='human'):
       # Renders the environment using a pyglet window
-      screen_width = 600
-      screen_height = 400
+      screen_width = 800
+      screen_height = 550
       line_x1 = 50
       line_x2 = 550
       line_y = 300
